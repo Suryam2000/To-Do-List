cc7017df9ec2ab029a7fdc5dad6f192594367c98
Suryam2000 <suryamvishwakarma8@gmail.com>
2022-07-03 03:09:20 +0530

    Created dir structure with express server and routes

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const port = 8000;
 
 const app = express();
-const router = require('./routes');
+
+app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err){console.log(`Error in running the server: ${err}`);}
