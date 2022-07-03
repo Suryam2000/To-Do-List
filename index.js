617b688db67eb849a03b179eecefad2f40ff10c2
Suryam2000 <suryamvishwakarma8@gmail.com>
2022-07-04 00:49:58 +0530

    Done some CSS

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const app = express();
 
 app.use('/', require('./routes'));
 
+app.use(express.static('assets'));
+
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(express.urlencoded());
 
 app.listen(port, function(err){
     if(err){console.log(`Error in running the server: ${err}`);}
